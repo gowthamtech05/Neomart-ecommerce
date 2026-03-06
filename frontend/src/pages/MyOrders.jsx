0fd49c6d4e09d6525a4265213dd000fe6457f158
Gowtham <gowthamtech05@gmail.com>
2026-03-06 18:37:04 +0530

    latest

--- frontend/src/pages/MyOrders.jsx
+++ frontend/src/pages/MyOrders.jsx
@@ -12,7 +12,7 @@ const MyOrders = () => {
     try {
       setLoading(true);
       const { data } = await axios.get(
-        "http://localhost:5000/api/orders/myorders",
+        `${import.meta.env.VITE_API_URL}/api/orders/myorders`,
         {
           withCredentials: true,
         },
@@ -53,7 +53,7 @@ const MyOrders = () => {
     try {
       setLoadingCancel((prev) => ({ ...prev, [orderId]: true }));
       await axios.put(
-        `http://localhost:5000/api/orders/${orderId}/cancel`,
+        `${import.meta.env.VITE_API_URL}/api/orders/${orderId}/cancel`,
         {},
         {
           withCredentials: true,
@@ -71,7 +71,7 @@ const MyOrders = () => {
   const downloadInvoice = async (orderId) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/api/orders/${orderId}/invoice`,
+        `${import.meta.env.VITE_API_URL}/api/orders/${orderId}/invoice`,
         {
           credentials: "include",
         },
