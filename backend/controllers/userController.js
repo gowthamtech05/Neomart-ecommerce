00aade552255b315c99d5db93053d075c73f9300
Gowtham <gowthamtech05@gmail.com>
2026-03-06 18:58:18 +0530

    latest

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,7 @@ const generateTokenAndSetCookie = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
@@ -112,7 +112,7 @@ export const logoutUser = (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     expires: new Date(0),
   });
   res.status(200).json({ message: "Logged out successfully" });
