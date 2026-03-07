64758e5d2eb9052a7ab546690bc7f4f3fc7e2749
Gowtham <gowthamtech05@gmail.com>
2026-03-07 18:53:00 +0530

    Ready

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,5 +5,5 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.get("/dashboard", protect, admin, getAdminDashboard);
-
+router.get("/orders/admin", protect, admin, getAdminDashboard);
 export default router;
