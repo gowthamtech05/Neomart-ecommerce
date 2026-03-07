f1949802e4ac77bb7bd7d5432e441813f47322dd
Gowtham <gowthamtech05@gmail.com>
2026-03-08 01:17:21 +0530

    Ready

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,7 @@ const generateTokenAndSetCookie = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
